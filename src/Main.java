cb4b2864ad0d520a4fd38054a9af709506c01912
moustapha diop <moustaphadiop@moustaphas-mbp.home>
2025-08-19 17:02:21 +0000

    nested if statements exercise

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,35 @@
 import java.util.Locale;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
-        /*tring name = "SpongeBob";
-        char firstLetter = 'S';
-        int age = 25;
-        double height = 1.5;
-        boolean isEmplyed = true;
+        boolean isStudent = false;
+        boolean isSenior=false;
+        double price = 9.99;
 
-        System.out.printf("Hello %s \n",name);
-        System.out.printf("Your name starts with a %c \n",firstLetter);
-        System.out.printf("Your are %d years old \n",age);
-        System.out.printf("Your are %.2f inches tall \n",height);
-        System.out.printf("Employed: %b \n",isEmplyed);
+        if (isStudent){
+            if(isSenior){
+                System.out.println("You get a senior discount 20%");
+                System.out.println("You get a student discount 10%");
+                price *= 0.7;
+                System.out.printf("the price of a ticket is : %.2f", price);
 
-        System.out.printf("%s is %d years old \n",name,age);*/
+            }else {
+                System.out.println("You get a student discount of 10%");
+                price *= 0.9;
+                System.out.printf("the price of a ticket is : %.2f", price);
 
-        double price1 = 9.99;
-        double price2 = 100.15;
-        double price3 = -54.01;
-        System.out.printf("%.1f\n",price1);
-        System.out.printf("%.1f\n",price2);
-        System.out.printf("%.1f\n",price3);
+            }
+        }else if(isSenior){
+            System.out.println("You get a senior discount of 20%");
+            price *= 0.8;
+            System.out.printf("the price of a ticket is : %.2f", price);
+        }else {
+            System.out.println("You get no discount of 0%");
+            System.out.printf("the price of a ticket is : %.2f", price);
+        }
 
-        System.out.printf("%.2f\n",price1);
-        System.out.printf("%.2f\n",price2);
-        System.out.printf("%.2f\n",price3);
-
-        System.out.printf("%+.2f\n",price1);
-        System.out.printf("%+.2f\n",price2);
-        System.out.printf("%+.2f\n",price3);
-
-        System.out.printf("%+(,.2f\n",price1);
-        System.out.printf("%+(,.2f\n",price2);
-        System.out.printf("%+(,.2f\n",price3);
-
-        int id1=1;
-        int id2=23;
-        int id3=453;
-        int id4=7678;
-
-        System.out.printf("%4d\n",id1);
-        System.out.printf("%4d\n",id2);
-        System.out.printf("%4d\n",id3);
-        System.out.printf("%2d\n",id4);
 
 
 
