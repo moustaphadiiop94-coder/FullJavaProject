6a661fb8394b6e65daa0dd8425e486fdbe4395f7
moustapha diop <moustaphadiop@moustaphas-mbp.home>
2025-08-19 15:49:07 +0000

    hypotenuse exercise

--- src/Main.java
+++ src/Main.java
@@ -5,34 +5,32 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        scanner.useLocale(Locale.US); // <-- accepte 2.0
+        scanner.useLocale(Locale.US);
+
+        /*double result ;
+        result = Math.pow(2,5);
+        result = Math.abs(-5);
+        result = Math.sqrt(9);
+        result = Math.ceil(3.14);
+        result = Math.floor(3.99);
+        result = Math.max(10 ,20);
+        System.out.println(result);*/
+
+        // hypotenuse c=Math.sqrt(a carre + b carre)
+        Locale.setDefault(Locale.US);
+        double a;
+        double b;
+        double c;
+
+        System.out.println("Enter the length of side A : ");
+        a = scanner.nextDouble();
+        System.out.println("Enter the length of side B : ");
+        b = scanner.nextDouble();
+
+        c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        System.out.println(" the hypotenuse (side c) is :" +c+"cm");
 
 
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("Hello " + name + "!");
-
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
-        System.out.println("You are " + age + " years old!");
-
-        System.out.print("What is your gpa: ");
-        double gpa = scanner.nextDouble();
-        System.out.println("Your GPA is: " + gpa);
-
-        // clear the newline left by nextDouble
-        scanner.nextLine();
-
-        System.out.print("Are you a student? (Y/N): ");
-        String studentInput = scanner.nextLine();
-
-        boolean isStudent = studentInput.equalsIgnoreCase("Y");
-
-        if (isStudent) {
-            System.out.println("You are enrolled as a student.");
-        } else {
-            System.out.println("You are not enrolled as a student.");
-        }
 
         scanner.close();
     }
