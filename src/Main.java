c2d5dafc0510fc9a9935bb9b40730ada646fc574
moustapha diop <moustaphadiop@moustaphas-mbp.home>
2025-08-19 18:27:17 +0000

    loop exercise 1

--- src/Main.java
+++ src/Main.java
@@ -2,52 +2,37 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        /*tring name = "SpongeBob";
-        char firstLetter = 'S';
-        int age = 25;
-        double height = 1.5;
-        boolean isEmplyed = true;
+    public static void main(String[] args) throws InterruptedException {
 
-        System.out.printf("Hello %s \n",name);
-        System.out.printf("Your name starts with a %c \n",firstLetter);
-        System.out.printf("Your are %d years old \n",age);
-        System.out.printf("Your are %.2f inches tall \n",height);
-        System.out.printf("Employed: %b \n",isEmplyed);
+        /*for (int i = 0 ; i <= 10 ; i ++){
+            System.out.println(i);
+        }
 
-        System.out.printf("%s is %d years old \n",name,age);*/
+        for (int i = 0 ; i <= 10 ; i +=2){
+            System.out.println(i);
+        }
 
-        double price1 = 9.99;
-        double price2 = 100.15;
-        double price3 = -54.01;
-        System.out.printf("%.1f\n",price1);
-        System.out.printf("%.1f\n",price2);
-        System.out.printf("%.1f\n",price3);
+        for (int i = 10 ; i >= 0 ; i -=2){
+            System.out.println(i);
+        }*/
 
-        System.out.printf("%.2f\n",price1);
-        System.out.printf("%.2f\n",price2);
-        System.out.printf("%.2f\n",price3);
+        Scanner scanner = new Scanner(System.in);
+        /*System.out.println("Enter how many times you want to loop ");
+        int times = scanner.nextInt();
 
-        System.out.printf("%+.2f\n",price1);
-        System.out.printf("%+.2f\n",price2);
-        System.out.printf("%+.2f\n",price3);
+        for (int i = 1; i <= times; i++) {
+            System.out.println("time " +i);
+        }*/
+        int start=10;
 
-        System.out.printf("%+(,.2f\n",price1);
-        System.out.printf("%+(,.2f\n",price2);
-        System.out.printf("%+(,.2f\n",price3);
+        for(int i= start ;i > 0;i--){
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
 
-        int id1=1;
-        int id2=23;
-        int id3=453;
-        int id4=7678;
+        System.out.println("Happy new year !");
 
-        System.out.printf("%4d\n",id1);
-        System.out.printf("%4d\n",id2);
-        System.out.printf("%4d\n",id3);
-        System.out.printf("%2d\n",id4);
-
-
-
+        scanner.close();
 
 
     }
