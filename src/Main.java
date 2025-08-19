b27030ffd20b4bc7e21543237d3eeb99a048da77
moustapha diop <moustaphadiop@moustaphas-mbp.home>
2025-08-19 23:10:25 +0000

    banking program

--- src/Main.java
+++ src/Main.java
@@ -2,53 +2,88 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+    static double balance =10.99;
+
+
     public static void main(String[] args) {
-        /*tring name = "SpongeBob";
-        char firstLetter = 'S';
-        int age = 25;
-        double height = 1.5;
-        boolean isEmplyed = true;
 
-        System.out.printf("Hello %s \n",name);
-        System.out.printf("Your name starts with a %c \n",firstLetter);
-        System.out.printf("Your are %d years old \n",age);
-        System.out.printf("Your are %.2f inches tall \n",height);
-        System.out.printf("Employed: %b \n",isEmplyed);
-
-        System.out.printf("%s is %d years old \n",name,age);*/
-
-        double price1 = 9.99;
-        double price2 = 100.15;
-        double price3 = -54.01;
-        System.out.printf("%.1f\n",price1);
-        System.out.printf("%.1f\n",price2);
-        System.out.printf("%.1f\n",price3);
-
-        System.out.printf("%.2f\n",price1);
-        System.out.printf("%.2f\n",price2);
-        System.out.printf("%.2f\n",price3);
-
-        System.out.printf("%+.2f\n",price1);
-        System.out.printf("%+.2f\n",price2);
-        System.out.printf("%+.2f\n",price3);
-
-        System.out.printf("%+(,.2f\n",price1);
-        System.out.printf("%+(,.2f\n",price2);
-        System.out.printf("%+(,.2f\n",price3);
-
-        int id1=1;
-        int id2=23;
-        int id3=453;
-        int id4=7678;
-
-        System.out.printf("%4d\n",id1);
-        System.out.printf("%4d\n",id2);
-        System.out.printf("%4d\n",id3);
-        System.out.printf("%2d\n",id4);
+        // JAVA BANKING PROGRAM
 
 
+        // DECLARE VARIABLES
+        boolean isRunning = true;
+        int choice;
+
+        //DISPLAY MENU
+        while (isRunning) {
+            System.out.println("************************");
+            System.out.println("Welcome to my BANK PROGRAM");
+            System.out.println("************************");
+            System.out.println("1. Show balance:");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice (1 - 4): ");
+            choice = scanner.nextInt();
+            // GET AND PROCESS USERS CHOICE
+            switch (choice) {
+                case 1 -> showBalance(balance);
+                case 2 -> balance += deposit();
+                case 3 -> balance -= withdraw();
+                case 4 -> isRunning = false;
+                default -> System.out.println("Invalid choice");
+            }
+
+        }
 
 
+        //EXIT MESSAGE
+
+        System.out.println("Thank you for using our program");
+
+        scanner.close();
 
     }
+    //SHOW BALANCE
+    static void showBalance(double balance) {
+        System.out.printf("$%.2f\n", balance);
+    }
+
+    // DEPOSIT
+    static double deposit() {
+        scanner.useLocale(Locale.US);
+        double amount;
+        System.out.print("Enter amount to deposit: ");
+        amount = scanner.nextDouble();
+
+        if (amount <= 0) {
+            System.out.println("Invalid amount");
+        }else {
+            return amount;
+        }
+        return 0;
+    }
+
+    //WITHDRAW
+    static double withdraw() {
+        scanner.useLocale(Locale.US);
+        double amount;
+        System.out.print("Enter amount to withdraw: ");
+        amount = scanner.nextDouble();
+        if (amount > balance) {
+            System.out.println("Insufficient funds");
+            return 0;
+
+        } else if (amount < 0) {
+            System.out.println("amount cant be negative");
+            return 0;
+        } else {
+            return amount;
+        }
+    }
+
+
+
 }
