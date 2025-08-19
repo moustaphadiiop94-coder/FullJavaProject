0f74c99bf98cf0ed0e45282a142dfb8783389fa0
moustapha diop <moustaphadiop@moustaphas-mbp.home>
2025-08-19 15:21:32 +0000

    if statements

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,46 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        scanner.useLocale(Locale.US); // <-- accepte 2.0
+        scanner.useLocale(Locale.US);
 
+        int age ;
+        String name ;
 
         System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("Hello " + name + "!");
+        name = scanner.nextLine();
+        
+        System.out.print("Are you a student (true/false) ? ");
+        boolean isStudent = scanner.nextBoolean();
+        //Group1
+        if(name.isEmpty()){
+            System.out.println("You did not enter a name.");
+        }else {
+            System.out.println(" Hello Your name is "+name);
+        }
 
         System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
-        System.out.println("You are " + age + " years old!");
+        age = scanner.nextInt();
 
-        System.out.print("What is your gpa: ");
-        double gpa = scanner.nextDouble();
-        System.out.println("Your GPA is: " + gpa);
 
-        // clear the newline left by nextDouble
-        scanner.nextLine();
+        //group 2
 
-        System.out.print("Are you a student? (Y/N): ");
-        String studentInput = scanner.nextLine();
+        if (age >= 65) {
+            System.out.println("You are a senior!");
+        }else if (age >= 18) {
+            System.out.println("You are an adult !");
+        }else if (age < 0) {
+            System.out.println(" you haven't born yet");
+        } else if (age == 0) {
+            System.out.println("You are a baby!");
+        }else {
+            System.out.println("you are a child");
+        }
 
-        boolean isStudent = studentInput.equalsIgnoreCase("Y");
-
-        if (isStudent) {
-            System.out.println("You are enrolled as a student.");
-        } else {
-            System.out.println("You are not enrolled as a student.");
+        // group 3
+        if(isStudent){
+            System.out.println("You are a student");
+        }else {
+            System.out.println("You are not a student");
         }
 
         scanner.close();
