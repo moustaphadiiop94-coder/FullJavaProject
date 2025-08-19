14fdc94048c62be4179c153011ad2c2512a5d4d4
moustapha diop <moustaphadiop@moustaphas-mbp.home>
2025-08-19 17:49:03 +0000

    logical operators exercise

--- src/Main.java
+++ src/Main.java
@@ -3,52 +3,41 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*tring name = "SpongeBob";
-        char firstLetter = 'S';
-        int age = 25;
-        double height = 1.5;
-        boolean isEmplyed = true;
+        /*double temperature= -20;
+        boolean isSunny = true;
 
-        System.out.printf("Hello %s \n",name);
-        System.out.printf("Your name starts with a %c \n",firstLetter);
-        System.out.printf("Your are %d years old \n",age);
-        System.out.printf("Your are %.2f inches tall \n",height);
-        System.out.printf("Employed: %b \n",isEmplyed);
+        if(temperature <= 30 && temperature >= 0 && isSunny){
+            System.out.println("The weather is good ");
+            System.out.println("Sunny");
+        } else if (temperature <=30 && temperature >=0 && !isSunny) {
+            System.out.println(" is not Sunny");
+            System.out.println("The weather is good ");
+        } else if (temperature > 30 || temperature < 0) {
+            System.out.println("The weather is bad ");
 
-        System.out.printf("%s is %d years old \n",name,age);*/
 
-        double price1 = 9.99;
-        double price2 = 100.15;
-        double price3 = -54.01;
-        System.out.printf("%.1f\n",price1);
-        System.out.printf("%.1f\n",price2);
-        System.out.printf("%.1f\n",price3);
+        }
+*/
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.printf("%.2f\n",price1);
-        System.out.printf("%.2f\n",price2);
-        System.out.printf("%.2f\n",price3);
+        //username must be between 4-12
+        //username must not contains spaces or underscores
 
-        System.out.printf("%+.2f\n",price1);
-        System.out.printf("%+.2f\n",price2);
-        System.out.printf("%+.2f\n",price3);
+        String username;
+        System.out.print("Enter ypur Username: ");
+        username = scanner.nextLine();
+        if(username.length() < 4 || username.length() > 12) {
+            System.out.println("Username ust be between 4-12");
+        } else if (username.contains(" ") && username.contains("_")) {
+            System.out.println("username must not contains spaces or underscores");
 
-        System.out.printf("%+(,.2f\n",price1);
-        System.out.printf("%+(,.2f\n",price2);
-        System.out.printf("%+(,.2f\n",price3);
-
-        int id1=1;
-        int id2=23;
-        int id3=453;
-        int id4=7678;
-
-        System.out.printf("%4d\n",id1);
-        System.out.printf("%4d\n",id2);
-        System.out.printf("%4d\n",id3);
-        System.out.printf("%2d\n",id4);
+        } else {
+            System.out.println("Username is valid "+username);
+        }
 
 
 
-
+        scanner.close();
 
     }
 }
