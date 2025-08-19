4696e5c44a4a500274b8f04b695904e8d4525ef4
moustapha diop <moustaphadiop@moustaphas-mbp.home>
2025-08-19 16:21:16 +0000

    printf exercise 2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String name = "SpongeBob";
+        /*tring name = "SpongeBob";
         char firstLetter = 'S';
         int age = 25;
         double height = 1.5;
@@ -15,7 +15,40 @@ public class Main {
         System.out.printf("Your are %.2f inches tall \n",height);
         System.out.printf("Employed: %b \n",isEmplyed);
 
-        System.out.printf("%s is %d years old \n",name,age);
+        System.out.printf("%s is %d years old \n",name,age);*/
+
+        double price1 = 9.99;
+        double price2 = 100.15;
+        double price3 = -54.01;
+        System.out.printf("%.1f\n",price1);
+        System.out.printf("%.1f\n",price2);
+        System.out.printf("%.1f\n",price3);
+
+        System.out.printf("%.2f\n",price1);
+        System.out.printf("%.2f\n",price2);
+        System.out.printf("%.2f\n",price3);
+
+        System.out.printf("%+.2f\n",price1);
+        System.out.printf("%+.2f\n",price2);
+        System.out.printf("%+.2f\n",price3);
+
+        System.out.printf("%+(,.2f\n",price1);
+        System.out.printf("%+(,.2f\n",price2);
+        System.out.printf("%+(,.2f\n",price3);
+
+        int id1=1;
+        int id2=23;
+        int id3=453;
+        int id4=7678;
+
+        System.out.printf("%4d\n",id1);
+        System.out.printf("%4d\n",id2);
+        System.out.printf("%4d\n",id3);
+        System.out.printf("%2d\n",id4);
+
+
+
+
 
     }
 }
