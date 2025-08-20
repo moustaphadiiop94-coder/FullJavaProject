706ddd3e661ef22ae36470b0ab4bc71006e2175b
moustapha diop <moustaphadiop@moustaphas-mbp.home>
2025-08-20 16:44:53 +0000

    Inheritance lesson 1

---
+++ src/Cat.java
@@ -0,0 +1,6 @@
+public class Cat extends Animal {
+    int lives = 9;
+    void speak(){
+        System.out.println("The cat goes *meow*");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,52 +3,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*tring name = "SpongeBob";
-        char firstLetter = 'S';
-        int age = 25;
-        double height = 1.5;
-        boolean isEmplyed = true;
-
-        System.out.printf("Hello %s \n",name);
-        System.out.printf("Your name starts with a %c \n",firstLetter);
-        System.out.printf("Your are %d years old \n",age);
-        System.out.printf("Your are %.2f inches tall \n",height);
-        System.out.printf("Employed: %b \n",isEmplyed);
-
-        System.out.printf("%s is %d years old \n",name,age);*/
-
-        double price1 = 9.99;
-        double price2 = 100.15;
-        double price3 = -54.01;
-        System.out.printf("%.1f\n",price1);
-        System.out.printf("%.1f\n",price2);
-        System.out.printf("%.1f\n",price3);
-
-        System.out.printf("%.2f\n",price1);
-        System.out.printf("%.2f\n",price2);
-        System.out.printf("%.2f\n",price3);
-
-        System.out.printf("%+.2f\n",price1);
-        System.out.printf("%+.2f\n",price2);
-        System.out.printf("%+.2f\n",price3);
-
-        System.out.printf("%+(,.2f\n",price1);
-        System.out.printf("%+(,.2f\n",price2);
-        System.out.printf("%+(,.2f\n",price3);
-
-        int id1=1;
-        int id2=23;
-        int id3=453;
-        int id4=7678;
-
-        System.out.printf("%4d\n",id1);
-        System.out.printf("%4d\n",id2);
-        System.out.printf("%4d\n",id3);
-        System.out.printf("%2d\n",id4);
-
-
-
-
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        System.out.println(" Dog is Alive "+dog.isAlive);
+        System.out.println(" Cat is Alive "+cat.isAlive);
+        System.out.println("Dog has "+dog.lives+" lives");
+        System.out.println("Cat has "+cat.lives+" lives");
+        dog.speak();
+        cat.speak();
 
     }
 }
