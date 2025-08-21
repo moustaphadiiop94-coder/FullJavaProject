0a14739319810bc9d55f79328022a5de455f1c61
moustapha diop <moustaphadiop@moustaphas-mbp.home>
2025-08-21 00:01:53 +0000

    read a file using java

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,35 @@
+import java.io.*;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*tring name = "SpongeBob";
-        char firstLetter = 'S';
-        int age = 25;
-        double height = 1.5;
-        boolean isEmplyed = true;
-
-        System.out.printf("Hello %s \n",name);
-        System.out.printf("Your name starts with a %c \n",firstLetter);
-        System.out.printf("Your are %d years old \n",age);
-        System.out.printf("Your are %.2f inches tall \n",height);
-        System.out.printf("Employed: %b \n",isEmplyed);
-
-        System.out.printf("%s is %d years old \n",name,age);*/
-
-        double price1 = 9.99;
-        double price2 = 100.15;
-        double price3 = -54.01;
-        System.out.printf("%.1f\n",price1);
-        System.out.printf("%.1f\n",price2);
-        System.out.printf("%.1f\n",price3);
-
-        System.out.printf("%.2f\n",price1);
-        System.out.printf("%.2f\n",price2);
-        System.out.printf("%.2f\n",price3);
-
-        System.out.printf("%+.2f\n",price1);
-        System.out.printf("%+.2f\n",price2);
-        System.out.printf("%+.2f\n",price3);
-
-        System.out.printf("%+(,.2f\n",price1);
-        System.out.printf("%+(,.2f\n",price2);
-        System.out.printf("%+(,.2f\n",price3);
-
-        int id1=1;
-        int id2=23;
-        int id3=453;
-        int id4=7678;
-
-        System.out.printf("%4d\n",id1);
-        System.out.printf("%4d\n",id2);
-        System.out.printf("%4d\n",id3);
-        System.out.printf("%2d\n",id4);
 
 
+        Locale.setDefault(Locale.US);
+        // how to read a file using java
+        //BufferedReader + FileReader : Best for reading text files line-by-line
+        //fileInputstream : Best for binary files (e.g images , audio files)
+        //RandomAccessFile : best for read/write specific portions of a large file
+
+        String filePath = "/Users/moustaphadiop/Desktop/testDEVBis.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))){
+
+            System.out.println("that file exists");
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+
+        }catch (FileNotFoundException e){
+            System.out.println("File not found");
+        }
+        catch (IOException e){
+            System.out.println("I/O exception");
+        }
 
 
 
